5290c9d7e440e71fdca5bfea7a84c7058ed3421b
ggoosen <georgegoosen@gmail.com>
2021-04-12 19:49:59 +0930

    Updated menu, included a pricing page for aurum

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -21,34 +21,45 @@ const config: MenuEntry[] = [
     ],
   },
   {
-    label: 'MIST Recipes',
+    label: 'Mist Info',
     icon: 'RecipeIcon',
-    href: 'https://www.alchemistdefi.com/farms',
+    items: [
+      {
+        label: 'Recipes',
+        href: 'https://www.alchemistdefi.com/farms',
+      },
+      {
+        label: 'Spells',
+        href: 'https://www.alchemistdefi.com/nests',
+      },
+      {
+        label: 'MIST Price Chart',
+        href: 'https://unidexbeta.app/bscCharting?token=0x6f8FE12CC34398d15b7D5A5BA933E550DA1D099f',
+      },    
+    ],
   },
   {
-    label: 'Spells',
-    icon: 'SpellIcon',
-    href: 'https://www.alchemistdefi.com/nests',
-  },
-  {
-    label: 'AURUM Alchemy',
+    label: 'Aurum Info',
     icon: 'PoolIcon',
-    href: 'https://www.aurum.alchemistdefi.com/vessels',
+    items: [
+      {
+        label: 'Enchanted Chamber',
+        href: 'https://www.aurum.alchemistdefi.com/aurum',
+      },
+      {
+        label: 'Aurum Enchantment',
+        href: 'https://www.aurum.alchemistdefi.com/vessels',
+      },
+      {
+        label: 'Aurum Price Chart',
+        href: 'https://unidexbeta.app/bscCharting?token=0x49207BAA3a7332F0716788aa57B088D499bcc104',
+      },    
+    ],
   },
   {
-    label: 'MIST Price Chart',
-    icon: 'ChartIcon',
-    href: 'https://unidexbeta.app/bscCharting?token=0x6f8FE12CC34398d15b7D5A5BA933E550DA1D099f',
-  },
-  {
-    label: 'Aurum Price Chart',
-    icon: 'ChartIcon',
-    href: 'https://unidexbeta.app/bscCharting?token=0x49207BAA3a7332F0716788aa57B088D499bcc104',
-  },
-  {
-    label: 'Project Documentation',
-    icon: 'DocIcon',
-    href: 'https://alchemist-defi.gitbook.io/alchemist-defi/',
+    label: 'TechRate Audit',
+    icon: 'AuditIcon',
+    href: 'https://alchemist-defi.com/TechrateAudit11042021',
   },  
   {
     label: 'More',
@@ -62,12 +73,11 @@ const config: MenuEntry[] = [
         label: 'Medium Blog',
         href: 'https://alchemistdefi.medium.com',
       },
+      {
+        label: 'Project Documentation',
+        href: 'https://alchemist-defi.gitbook.io/alchemist-defi/',
+      },      
     ],
-  },  
-  {
-    label: 'TechRate Audit',
-    icon: 'AuditIcon',
-    href: 'https://alchemist-defi.com/TechrateAudit11042021',
   },  
 ]
 
