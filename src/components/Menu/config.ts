93b6cba38787b8d6e917bb490b9907f29fa27bb1
ggoosen <georgegoosen@gmail.com>
2021-04-12 20:19:25 +0930

    updated pdf link

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -59,7 +59,7 @@ const config: MenuEntry[] = [
   {
     label: 'TechRate Audit',
     icon: 'AuditIcon',
-    href: 'https://alchemist-defi.com/TechrateAudit11042021.pdf',
+    href: 'https://alchemist-defi.com/files/TechrateAudit11042021.pdf',
   },  
   {
     label: 'More',
